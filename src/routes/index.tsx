53a796ef07780c357bf597000543887383cf20dc
Youssefhazem <92303440+Youssefhazem@users.noreply.github.com>
2022-12-17 21:27:06 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello joe</Title>
-      <h1>Hello world!</h1>
+      <h1>Hello Joe</h1>
       <Counter />
       <p>
         Visit{" "}
